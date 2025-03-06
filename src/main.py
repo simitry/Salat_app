3406609ff1acd584a58a5027f018eae1f0cb7ffd
Simitry <saadmakkouch2003@gmail.com>
2025-03-06 03:28:04 +0000

    added notification and while loop for setting up a service

--- src/main.py
+++ src/main.py
@@ -1,20 +1,49 @@
 # from connect import create_table as ct
 from salat_api import *
 from datetime import datetime
+import signal
+from time import sleep
+from plyer import notification
 
+class SignalHandler:
+    shutdown_requested =False
+    
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.request_shutdown)
+        signal.signal(signal.SIGTERM , self.request_shutdown)
+    
+    def request_shutdown(self, *args):
+        print("Request to shutdown, ........... stopping")
+        self.shutdown_requested = True
+        
+    def can_run(self):
+        return not self.shutdown_requested
+    
+    
 def compare():
-    # ct("" , "test", "test3")
     salat_name = ["Fajr","Sunrise","Dhuhr","Asr","Maghrib","Isha"]
 
-    print(response)
-    print(timing)
+    # print(response)
+    # print(timing)
 
     for elem in salat_name:
         if timing[elem] == datetime.now().strftime("%H:%M"): #strftime to customize output
-            print(f'time for {elem}')
+            notification.notify(
+                title='Salat-App',
+                message=f'time for {elem}',
+                app_name ='Salat-App',
+                app_icon = 'img/icon.ico',
+                timeout=10
+            )
+            sleep (60)
+            
+ 
+signal_handler = SignalHandler()    
 
-    print(datetime.today())
+print("Program running. Press Ctrl+C to stop.")
+
+while signal_handler.can_run():  
+    compare()
+    sleep(2)
     
-    
-compare()
-# ct("" , "test", "test4")
+print("Program has stopped.")
